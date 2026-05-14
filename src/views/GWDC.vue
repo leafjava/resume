ac7e3e9df795dfd6601bb834f237cdd168bc9ddb
LAPTOP-5HJABA2M\Administer <a894074295@126.com>
2026-05-15 01:51:16 +0800

    内容调整

--- src/views/GWDC.vue
+++ src/views/GWDC.vue
@@ -1,5 +1,5 @@
 <template>
-  <div>
+  <div class="gwdc-page">
     <h1>GWDC</h1>
     <p>这里是 GWDC 页面的内容。</p>
   </div>
@@ -10,5 +10,20 @@ export default {
 }
 </script>
 <style lang="scss" scoped>
-  
+.gwdc-page {
+  padding-top: 100px; /* 给 navbar 留出空间 */
+  min-height: 100vh;
+  padding-left: 2rem;
+  padding-right: 2rem;
+}
+
+h1 {
+  font-size: 3rem;
+  margin-bottom: 1rem;
+}
+
+p {
+  font-size: 1.2rem;
+  color: var(--color-text-secondary);
+}
 </style>
