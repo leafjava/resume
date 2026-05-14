aabac4c040f29cac041012c46988bd1248a923df
LAPTOP-5HJABA2M\Administer <a894074295@126.com>
2026-05-15 01:26:59 +0800

    路由配置

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,13 @@
 <template>
   <div id="app">
     <Navbar />
-    <main>
-      <Hero />
-      <Achievements />
-      <Speaking />
-      <Projects />
-      <TechStack />
-      <About />
-      <Contact />
-    </main>
+    <router-view />
     <FloatingButton />
   </div>
 </template>
 
 <script setup>
 import Navbar from './components/Navbar.vue'
-import Hero from './components/Hero.vue'
-import Achievements from './components/Achievements.vue'
-import Speaking from './components/Speaking.vue'
-import Projects from './components/Projects.vue'
-import TechStack from './components/TechStack.vue'
-import About from './components/About.vue'
-import Contact from './components/Contact.vue'
 import FloatingButton from './components/FloatingButton.vue'
 </script>
 
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,21 @@
-<script setup>
-import TheWelcome from '../components/TheWelcome.vue'
-</script>
-
 <template>
   <main>
-    <TheWelcome />
+    <Hero />
+    <Achievements />
+    <Speaking />
+    <Projects />
+    <TechStack />
+    <About />
+    <Contact />
   </main>
 </template>
+
+<script setup>
+import Hero from '../components/Hero.vue'
+import Achievements from '../components/Achievements.vue'
+import Speaking from '../components/Speaking.vue'
+import Projects from '../components/Projects.vue'
+import TechStack from '../components/TechStack.vue'
+import About from '../components/About.vue'
+import Contact from '../components/Contact.vue'
+</script>
